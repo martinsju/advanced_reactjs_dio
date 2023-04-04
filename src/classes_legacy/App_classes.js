7e076bc2e40eb61a7e8129119d5c6e824338f7c2
julia <agramartinsj@gmail.com>
2023-04-04 18:06:15 -0300

    refactor: moved legacy files but keep it to study

---
+++ src/classes_legacy/App_classes.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import Twitter from '../Twitter'
+
+class App extends Component {
+	state = {
+		loading: false,
+		actived: true
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount App')
+		setTimeout(() => {
+			this.setState({
+				loading: true
+			})
+		}, 2000)
+	}
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate App')
+	}
+
+	componentWillUnmount() {
+		console.log('componentWillUnmount App')
+	}
+
+	onRemove = () => {
+		this.setState({
+			actived: false
+		})
+	}
+
+	onRecover = () => {
+		this.setState({
+			actived: true
+		})
+	}
+
+	render() {
+		const posts = [
+			{
+				content: 'lalala',
+				date: '03/04/2023'
+			},
+			{
+				content: 'lalala',
+				date: '03/04/2023'
+			}
+		]
+
+		return (
+			<div>
+				{this.state.actived && (
+					<Twitter posts={posts} loading={this.state.loading} />
+				)}
+				<button onClick={this.onRemove}>Remove component</button>
+				<button onClick={this.onRecover}>Recover component</button>
+			</div>
+		)
+	}
+}
+
+export default App
