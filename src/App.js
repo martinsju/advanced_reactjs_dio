b3371822a2e223d9e056c043fa069254459bc2ec
julia <agramartinsj@gmail.com>
2023-04-04 19:06:59 -0300

    fix: fixed loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ThemeContext, themes } from './components/ThemeSwitcher'
 import Twitter from './components/TwitterPage/Twitter'
 
 function App() {
+	const [posts, setPosts] = useState(
+		{
+			content: 'this is my tweet content',
+			author: 'Julia Agra'
+		},
+		{
+			content: 'this is my tweet content',
+			author: 'John Doe'
+		}
+	)
+
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+		}, 3000)
+		console.log('useeffect do app rodou e loading é ', loading)
+	}, [])
+
 	return (
 		<ThemeContext.Provider value={themes.primary}>
-			<Twitter />
+			<Twitter posts={posts} loading={loading} />
+			<p>load: {loading.toString()}</p>
+			<button>[unabled] Remove Tweet</button>
+			<button>[unabled] Recover Tweet</button>
 		</ThemeContext.Provider>
 	)
 }
