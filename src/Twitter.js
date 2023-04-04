b23d4bc873709ed698d1c7e44f142f42748b74e8
julia <agramartinsj@gmail.com>
2023-04-04 18:06:56 -0300

    refactor: rewrite components to functions

--- src/Twitter.js
+++ src/Twitter.js
@@ -1,35 +1,42 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from './Theme'
 
-class Twitter extends Component {
-	state = {
-		tweet: true
-	}
+function Twitter() {
+	const theme = useContext(ThemeContext)
+	// state = {
+	// 	tweet: true
+	// }
 
-	componentDidMount() {
-		const { posts, loading } = this.props
-		console.log('componentDidMount Twitter', posts)
-		console.log('componentDidMount loading: ', loading)
-	}
+	// componentDidMount() {
+	// 	const { posts, loading } = this.props
+	// 	console.log('componentDidMount Twitter', posts)
+	// 	console.log('componentDidMount loading: ', loading)
+	// }
 
-	componentDidUpdate() {
-		const { loading } = this.props
-		console.log('componentDidUpdate Twitter loading: ', loading)
-	}
+	// componentDidUpdate() {
+	// 	const { loading } = this.props
+	// 	console.log('componentDidUpdate Twitter loading: ', loading)
+	// }
 
-	componentWillUnmount() {
-		const { loading } = this.props
-		console.log('componentWillUnmount Twitter loading: ', loading)
-	}
+	// componentWillUnmount() {
+	// 	const { loading } = this.props
+	// 	console.log('componentWillUnmount Twitter loading: ', loading)
+	// }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		//anything I want the component to re-render
-		// return this.state.tweet !== nextState.tweet
-		return false
-	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	//anything I want the component to re-render
+	// 	// return this.state.tweet !== nextState.tweet
+	// 	return false
+	// }
 
-	render() {
-		return <div>this is my Twitter class component </div>
-	}
+	return (
+		<div style={{ background: theme.background, color: theme.color }}>
+			this is my Twitter functional component
+			<button style={{ backgroundColor: theme.background, color: theme.color }}>
+				Change Theme
+			</button>
+		</div>
+	)
 }
 
 export default Twitter
